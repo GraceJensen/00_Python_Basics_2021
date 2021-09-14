37ac33370f251f4154f3b057d3fc43468dbdc4a9
GraceJensen <gjensen@tgc.school.nz>
2021-09-15 11:54:55 +1200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,14 @@
+# code to learn about variables
+
+#variables can be string
+greeting = "hello world"
+
+# they can also be numbers
+fav_num = 7
+
+# this variable has a problem
+my_name = "Miss G"
+
+print(greeting)
+print(fav_num)
+print(my_name)
